a8571970e488990f6193d555a40091a8cff01d9a
Ignacio Mattiauda <naccho.mattiauda@gmail.com>
2022-08-16 21:22:26 -0300

    add enter key input function

--- js/main.js
+++ js/main.js
@@ -17,51 +17,56 @@ window.addEventListener("load", () => {
     }
   }
   function searchBar() {
-    $(".searchButton").click((event) => {
-      let input = textNormalize($("#pizzaSearchInput").val());
-      if (input != undefined) {
-        const lista = document.querySelectorAll(".pizzaContainer");
-        let foundStatus = false;
-        lista.forEach((e, i) => {
-          if (textNormalize(e.querySelector("h3").textContent) === input) {
-            if (e.classList.contains("displayNone"))
+    function search(){
+        let input = textNormalize($("#pizzaSearchInput").val());
+        if (input != undefined) {
+          const lista = document.querySelectorAll(".pizzaContainer");
+          let foundStatus = false;
+          lista.forEach((e, i) => {
+            if (textNormalize(e.querySelector("h3").textContent) === input) {
+              if (e.classList.contains("displayNone"))
+                e.classList.remove("displayNone");
+                foundStatus = true;
+                return
+            } 
+            else{
+              e.querySelector("h3").parentNode.parentNode.classList.add(
+                "displayNone");
+                console.log("primer else")
+                if(textNormalize(e.querySelector("h3").textContent) !== input && i == lista.length - 1 && foundStatus == false) {
+                  console.log("if")
+                  $("main").append($(`<div class="errorContainer centerFlex"><p class="errorMessage">No se han encontrado resultados en tu búsqueda.</p><button class="errorButton">Quitar Búsqueda</button></div>`));
+                  $(".listTitle").addClass("displayNone");
+                  $(".errorButton").click((event) => {
+                      $(".errorContainer").remove();
+                      const lista = document.querySelectorAll(".pizzaContainer");
+                      lista.forEach((e, i) => {
+                        if (e.classList.contains("displayNone")) {
+                          e.classList.remove("displayNone");
+                          $(".listTitle").removeClass("displayNone");
+                          $("#pizzaSearchInput").val("")
+                        }
+                      });
+                  })
+                };
+              }; 
+            
+  
+          });
+        } else {
+          const lista = document.querySelectorAll(".pizzaContainer");
+          lista.forEach((e, i) => {
+            if (e.classList.contains("displayNone")) {
               e.classList.remove("displayNone");
-              foundStatus = true;
-              return
-          } 
-          else{
-            e.querySelector("h3").parentNode.parentNode.classList.add(
-              "displayNone");
-              console.log("primer else")
-              if(textNormalize(e.querySelector("h3").textContent) !== input && i == lista.length - 1 && foundStatus == false) {
-                console.log("if")
-                $("main").append($(`<div class="errorContainer centerFlex"><p class="errorMessage">No se han encontrado resultados en tu búsqueda.</p><button class="errorButton">Quitar Búsqueda</button></div>`));
-                $(".listTitle").addClass("displayNone");
-                $(".errorButton").click((event) => {
-                    $(".errorContainer").remove();
-                    const lista = document.querySelectorAll(".pizzaContainer");
-                    lista.forEach((e, i) => {
-                      if (e.classList.contains("displayNone")) {
-                        e.classList.remove("displayNone");
-                        $(".listTitle").removeClass("displayNone");
-                        $("#pizzaSearchInput").val("")
-                      }
-                    });
-                })
-              };
-            }; 
-          
-
-        });
-      } else {
-        const lista = document.querySelectorAll(".pizzaContainer");
-        lista.forEach((e, i) => {
-          if (e.classList.contains("displayNone")) {
-            e.classList.remove("displayNone");
-          }
-        });
-        return;
+            }
+          });
+          return;
+        }
       }
+    $("input").keypress(function(event) {
+        if (event.which == 13) search();
+         
     });
+    $(".searchButton").click((event) => search());
   }
 });
